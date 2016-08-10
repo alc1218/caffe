da127299cc83871ca05032a824ed9cc7d80efe7f
alc1218 <alc1218@gmail.com>
2016-08-10 18:35:04 +0200

    plot loss curve updated

--- taza_tools/plot_tazacaffe_trainlog.py
+++ taza_tools/plot_tazacaffe_trainlog.py
@@ -41,14 +41,23 @@ if __name__=="__main__":
         #thisit_testloss = dict([strfloat(re.findall('(loss[@a-zA-Z0-9\/ _\-]+) = ([0-9\.e\-]+) ', y)[0]) for y in iterdic[x] if 'Test net output' in y and ': loss' in y])
         #thisit_testacc = dict([strfloat(re.findall('(accuracy[@a-zA-Z0-9\/ _\-]+) = ([0-9\.e\-]+)', y)[0]) for y in iterdic[x] if 'Test net output' in y and ': accuracy' in y])
 
-        thisit_trainloss = dict([strfloat(re.findall('(loss[@a-zA-Z0-9\/ _\-]*) = ([0-9\.e\-]+) ', y)[0]) for y in iterdic[x] if 'Train net output' in y and ': loss' in y])
-        thisit_testloss = dict([strfloat(re.findall('(loss[@a-zA-Z0-9\/ _\-]*) = ([0-9\.e\-]+) ', y)[0]) for y in iterdic[x] if 'Test net output' in y and ': loss' in y])
-        thisit_testacc = dict([strfloat(re.findall('(accuracy[@a-zA-Z0-9\/ _\-]*) = ([0-9\.e\-]+)', y)[0]) for y in iterdic[x] if 'Test net output' in y and ': accuracy' in y])
+        '''
+        for y in iterdic[x]:
+            if 'Train net output' in y and ': loss' in y:
+                print str(re.findall('loss = ([0-9].[0-9]*)', y)[0])
+                #import ipdb; ipdb.set_trace()
+                print str(dict([strfloat(re.findall('(loss) = ([0-9\.]+)', y)[0])]))
+                thisit_trainloss = dict[(float(re.findall('(loss) = ([0-9\.]+)', y)[0])])
+        '''
 
+        thisit_trainloss = dict([strfloat(re.findall('(loss) = ([0-9\.]+)', y)[0]) for y in iterdic[x] if 'Train net output' in y and ': loss' in y])
+        thisit_testloss = dict([strfloat(re.findall('(loss) = ([0-9\.]+)', y)[0]) for y in iterdic[x] if 'Test net output' in y and ': loss' in y])
+        thisit_testacc = dict([strfloat(re.findall('(accuracy) = ([0-9\.]+)', y)[0]) for y in iterdic[x] if 'Test net output' in y and ': accuracy' in y])
+        
         #full_train_loss = dict([strfloat(re.findall('(loss) = ([0-9\.]+)', iterdic[x][0])[0])]) if 'loss' in iterdic[x][0] else None
 
-        print "len(thisit_trainloss): " + str(len(thisit_trainloss))
-        print "x: " + str(iterdic[x])
+        #print "len(thisit_trainloss): " + str(len(thisit_trainloss))
+        #print "x: " + str(iterdic[x])
 
         iterscores[int(x)] = {}
         if len(thisit_trainloss):
@@ -70,8 +79,10 @@ if __name__=="__main__":
     #plots:
     linestyles = cycle(['r', 'g', 'b', 'y', 'k', 'c', 'm', 'r-+', 'g-+', 'b-+', 'y-+', 'k-+', 'c-+', 'm-+', 'r-o', 'g-o', 'b-o', 'y-o', 'k-o', 'c-o', 'm-o', 'r-^', 'g-^', 'b-^', 'y-^', 'k-^', 'c-^', 'm-^'])
     #1) test accuracy
+    '''
     leg = []
     pylab.figure(1)
+    pylab.ylim([0,1])
     pylab.title('Validation accuracy')
     pylab.xlabel('iteration')
     pylab.ylabel('accuracy')
@@ -119,6 +130,129 @@ if __name__=="__main__":
     leg = ['joint loss']
     axarray[j][i].legend(leg)
     pylab.show()
+    '''
+
+
+
+
+
+
+    '''
+    leg = []
+    pylab.figure(1)
+    pylab.ylim([0,1])
+    pylab.title('Validation accuracy')
+    pylab.xlabel('iteration')
+    pylab.ylabel('accuracy')
+    pylab.hold(1)
+    for k in names_testacc:
+        leg.append(k)
+        X, Y = map(np.array, zip(*[(int(x), iterscores[x]['testacc'][k]) for x in iterscores if 'testacc' in iterscores[x]]))
+        order = np.argsort(X)
+        X = X[order]
+        Y = Y[order]
+        pylab.plot(X,Y, linestyles.next())
+    pylab.legend(leg)
+    #2) train vs test loss
+    linestyles = cycle(['rs', 'gs', 'bs', 'ys', 'ks', 'cs', 'ms', 'r+', 'g+', 'b+', 'y+', 'k+', 'c+', 'm+', 'ro', 'go', 'bo', 'yo', 'ko', 'co', 'mo', 'r^', 'g^', 'b^', 'y^', 'k^', 'c^', 'm^'])
+    sideplots = int(np.ceil(np.sqrt(len(names_testloss)+1)))
+    f, axarray = pylab.subplots(sideplots)
+    f.suptitle('train/val loss')
+    for j in range(sideplots):
+        if (j*sideplots) >= len(names_testloss):
+            continue
+        k = names_testloss[(j*sideplots)]
+        leg = []
+        leg.append('train '+k)
+        X, Y = map(np.array, zip(*[(int(x), iterscores[x]['trainloss'][k]) for x in iterscores if 'trainloss' in iterscores[x] and iterscores[x]['trainloss'][k]!=0]))
+        order = np.argsort(X)
+        X = X[order]
+        Y = Y[order]
+        axarray[j].plot(X/1000.,Y, linestyles.next())
+        leg.append('test '+k)
+        X, Y = map(np.array, zip(*[(int(x), iterscores[x]['testloss'][k]) for x in iterscores if 'testloss' in iterscores[x] and iterscores[x]['testloss'][k]!=0]))
+        order = np.argsort(X)
+        X = X[order]
+        Y = Y[order]
+        axarray[j].plot(X/1000.,Y, linestyles.next())
+        axarray[j].legend(leg)
+        axarray[j].set_xlabel('iteration (*1000)')
+        axarray[j].set_ylabel('loss')
+
+    X, Y = map(np.array, zip(*[(int(x), iterscores[x]['trainloss']['loss']) for x in iterscores if 'trainloss' in iterscores[x] and 
+                               'loss' in iterscores[x]['trainloss'] and iterscores[x]['trainloss']['loss']!=0]))
+    order = np.argsort(X)
+    X = X[order]
+    Y = Y[order]
+    axarray[j].plot(X/1000.,Y, linestyles.next())
+    leg = ['joint loss']
+    axarray[j].legend(leg)
+
+    axarray[0].set_ylim([0,5])
+    axarray[1].set_ylim([0,5])
+
+    pylab.show()
+    '''
+
+    if len(names_testacc) > 0:
+
+        pylab.figure(1)
+        pylab.ylim([0,1])
+        pylab.title('Validation accuracy')
+        pylab.xlabel('iteration')
+        pylab.ylabel('accuracy')
+        pylab.hold(1)
+        leg = []
+        for k in names_testacc:
+            leg.append(k)
+            X, Y = map(np.array, zip(*[(int(x), iterscores[x]['testacc'][k]) for x in iterscores if 'testacc' in iterscores[x]]))
+            order = np.argsort(X)
+            X = X[order]
+            Y = Y[order]
+            pylab.plot(X,Y, linestyles.next())
+        pylab.legend(leg)
+
+    #2) train vs test loss
+    if len(names_trainloss) > 0 or len(names_testloss) > 0:
+        pylab.figure(2)
+        pylab.clf()
+        #pylab.plot(X/1000.,Y, linestyles.next())
+        pylab.title('train/val loss')
+        #pylab.xlabel('iteration (*1000)')
+        pylab.xlabel('iteration')
+        pylab.ylabel('loss')
+
+        #k = names_testloss[(0)]
+        k = 'loss'
+        leg = []
+
+        for k in names_trainloss:
+            #TRAIN SET
+            leg.append('train '+k)
+            X, Y = map(np.array, zip(*[(int(x), iterscores[x]['trainloss'][k]) for x in iterscores if 'trainloss' in iterscores[x] and iterscores[x]['trainloss'][k]!=0]))
+            order = np.argsort(X)
+            X = X[order]
+            Y = Y[order]
+            #pylab.plot(X/1000.,Y, linestyles.next())
+            pylab.plot(X,Y, linestyles.next())
+
+        for k in names_testloss:
+            #TEST SET
+            leg.append('test '+k)
+            X, Y = map(np.array, zip(*[(int(x), iterscores[x]['testloss'][k]) for x in iterscores if 'testloss' in iterscores[x] and iterscores[x]['testloss'][k]!=0]))
+            order = np.argsort(X)
+            X = X[order]
+            Y = Y[order]
+            #pylab.plot(X/1000.,Y, linestyles.next())
+            pylab.plot(X,Y, linestyles.next())
+        
+        pylab.legend(leg)
+        pylab.ylim([0,5]) # SOFTMAX
+        pylab.ylim([0,0.5]) # CONTRASTIVE
+
+    if len(names_testacc) > 0 or len(names_trainloss) > 0 or len(names_testloss) > 0:
+
+        pylab.show()
 
 
 
